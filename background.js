ab4701629f1c7caceafabef370ae99a0dff5a6ea
akibayashi <akibayashi@qq.com>
2018-02-28 07:57:02 +0800

    Update background.js

--- background.js
+++ background.js
@@ -4,7 +4,12 @@ function getVideoCover(info, tab)
     	if(response.coverImageURL)
     		chrome.tabs.create({"url":response.coverImageURL});
     	else
-			alert("没有找到封面!可能由以下原因导致：\n1、此视频不存在。\n2、此页面并不是标准的视频页面。\n3、本插件出了点问题。\n4、bilibili更改了封面链接的写法。\n对于以上第3、4种情况，可邮件联系我：akibayashi@qq.com");  
+			alert("没有找到封面!可能由以下原因导致：\n"+
+			"1、此视频不存在。\n"+
+			"2、url后有#replyxxx等多余部分，请删掉再试\n"+
+			"3、本插件出了点问题。\n"+
+			"4、bilibili更改了封面链接的写法。\n"+
+			"如有疑问，可在github上与我交流：https://github.com/akimusume/GetBilibiliVideoCover/");  
 	}); 
 }
 var menu=chrome.contextMenus.create({
