495bfff3184bd1ca114dd5d9c1e61971094111b3
akibayashi <akibayashi@qq.com>
2018-07-09 20:14:59 +0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,7 +1,17 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	var coverImage=document.getElementsByClassName("cover_image");
-	if(coverImage.length!=0)
-		sendResponse({coverImageURL:coverImage[0].currentSrc});
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+	//console.log("onMessage");
+	metas = document.getElementsByTagName('meta');
+	var imageurl;
+	if (metas[9].getAttribute("itemprop") == "image")
+		imageurl = metas[9].content;
 	else
-		sendResponse({coverImageURL:""});
+		for (var i = 0; i < matas.length; i++)
+			if (metas[i].getAttribute("itemprop") == "image") {
+				imageurl = metas[i].content;
+				break;
+			}
+	console.log(imageurl);
+	sendResponse({
+		coverImageURL: imageurl
+	})
 });
